8d08be2de4f04058d40ec056d289bce26edc225f
sunzhongshao <877120376@qq.com>
2023-06-14 22:59:16 +0800

    BackUp

---
+++ Assets/Scripts/LevelScripts/Level1-1/RotationBird.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationBird : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = (target.position+new Vector3(0,.2f,0)) - transform.position;
+
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 3 * Time.deltaTime);
+    }
+}
