b590eeca20eb854fa7570859d650941db004135b
sunzhongshao <877120376@qq.com>
2023-06-19 23:37:00 +0800

    Updates Chains

---
+++ Assets/MGS.Packages/Machinery/Rutime/Crank/LimitCrank.cs
@@ -0,0 +1,71 @@
+﻿/*************************************************************************
+ *  Copyright © 2015-2018 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  LimitCrank.cs
+ *  Description  :  Define LimitCrank component.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  4/17/2018
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEngine;
+
+namespace MGS.Machineries
+{
+    /// <summary>
+    /// Crank rotate around the axis Z in the limit range.
+    /// </summary>
+    public class LimitCrank : FreeCrank
+    {
+        #region Field and Property
+        /// <summary>
+        /// Range limit of angle.
+        /// </summary>
+        [Tooltip("Range limit of angle.")]
+        public Range range = new Range(-45, 45);
+
+        /// <summary>
+        /// Mechanism is stuck?
+        /// </summary>
+        public override bool IsStuck
+        {
+            get
+            {
+                if (CheckAngleStuck())
+                {
+                    return true;
+                }
+                return base.IsStuck;
+            }
+        }
+        #endregion
+
+        #region Protected Method
+        /// <summary>
+        /// Check current angle is stuck.
+        /// </summary>
+        /// <returns>Angle is stuck?</returns>
+        protected bool CheckAngleStuck()
+        {
+            return Angle <= range.min || Angle >= range.max;
+        }
+
+        /// <summary>
+        /// Drive mechanism by velocity.
+        /// </summary>
+        /// <param name="velocity">Velocity of drive.</param>
+        /// <param name="mode">Mode of drive.</param>
+        /// <returns>Drive is effective?</returns>
+        protected override bool OnDrive(float velocity, DriveMode mode)
+        {
+            Angle += velocity;
+            Angle = Mathf.Clamp(Angle, range.min, range.max);
+
+            RotateCrank(Angle);
+            return DriveRockers(velocity, mode);
+        }
+        #endregion
+    }
+}
---
+++ Assets/MGS.Packages/Machinery/Rutime/Gear/Axle.cs
@@ -0,0 +1,69 @@
+﻿/*************************************************************************
+ *  Copyright © 2017-2018 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  Axle.cs
+ *  Description  :  Define Axle component.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  6/5/2018
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEngine;
+
+namespace MGS.Machineries
+{
+    /// <summary>
+    /// Axle rotate around axis Z.
+    /// </summary>
+    public class Axle : CoaxeMechanism
+    {
+        #region Field and Property
+        /// <summary>
+        /// Mechanism is stuck?
+        /// </summary>
+        public override bool IsStuck
+        {
+            get
+            {
+                if (CheckCoaxeStuck())
+                {
+                    return true;
+                }
+                return base.IsStuck;
+            }
+        }
+        #endregion
+
+        #region Protected Method
+        /// <summary>
+        /// Check one of the coaxes is stuck?
+        /// </summary>
+        /// <returns>Return true if one of the coaxes is stuck.</returns>
+        protected bool CheckCoaxeStuck()
+        {
+            foreach (var coaxe in coaxes)
+            {
+                if (coaxe.IsStuck)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Drive mechanism by velocity.
+        /// </summary>
+        /// <param name="velocity">Velocity of drive.</param>
+        /// <param name="mode">Mode of drive.</param>
+        /// <returns>Drive is effective?</returns>
+        protected override bool OnDrive(float velocity, DriveMode mode = DriveMode.Ignore)
+        {
+            transform.Rotate(Vector3.forward, velocity, Space.Self);
+            return DriveCoaxes(velocity);
+        }
+        #endregion
+    }
+}
